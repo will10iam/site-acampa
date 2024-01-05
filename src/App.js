3827191a50ddbbdc9195710080f928e6409537ea
William Berbet <wiiliamb@hotmail.com>
2024-01-05 07:44:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/title';
+import Counter from './components/counter';
+
+import BACKG from './assets/bgacampa.png'
+
+import useCountdown from './hooks/useCountdown';
+
 
 function App() {
+  const [day, hour, minute, second] = useCountdown('Feb 10, 2024 08:00:00');
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${BACKG})` }}>
+      <div className='container'>
+        <Title title='ACAMPA2024 começa em..' />
+        <div className='countdown-container'>
+          <Counter title='Dias' number={day} />
+          <Counter title='Horas' number={hour} />
+          <Counter title='Minutos' number={minute} />
+          <Counter title='Segundos' number={second} />
+        </div>
+
+        <div className='action'>
+          <a href='https://forms.gle/qbe9VkYMyZH6QiPs6'><button type='text'>garanta já a sua vaga</button></a>
+          <p>clique no botão para fazer a inscrição!</p>
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/title.js
@@ -0,0 +1,8 @@
+import './title.css'
+import React from 'react'
+
+export default function Title({ title }) {
+    return (
+        <h1 className='title'>{title}</h1>
+    )
+}
