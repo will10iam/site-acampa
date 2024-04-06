8d8543a08f05658d647577e6449d2f8753f98f97
William Berbet <wiiliamb@hotmail.com>
2024-04-06 13:43:04 -0300

    finalizando inputs + página de obrigado

--- src/pages/Registration/index.js
+++ src/pages/Registration/index.js
@@ -1,15 +1,21 @@
 /* global firebase */
 
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../index.css'
+import { FiUpload } from 'react-icons/fi'
+
 
 import firebase from 'firebase/compat/app'; // Importa o módulo 'app' compatível
 /* import 'firebase/compat/database'; // Importa o módulo 'database' compatível*/
 import Title from '../../components/title';
+import logo from "../../assets/construide_logo.png"
+import avatar from '../../assets/avatar.png'
 
 /* import { initializeApp } from 'firebase/app' */
 import { getDatabase, ref, push } from 'firebase/database'
 import { getStorage, ref as storageRef, uploadBytes } from 'firebase/storage'
+import Button from '../../components/button';
 
 
 const firebaseConfig = {
@@ -43,6 +49,8 @@ function RegistrationForm() {
         payment: '',
     });
 
+    const navigate = useNavigate();
+
     const handleChange = (e) => {
 
         if (e.target.name === 'profileImage') {
@@ -65,35 +73,42 @@ function RegistrationForm() {
         const storage = getStorage();
         const storageRefe = storageRef(storage, formData.profileImage.name);
 
-        uploadBytes(storageRefe, formData.profileImage)
-            .then((snapshot) => {
-                console.log('Imagem de perfil enviada com sucesso:', snapshot.ref.fullPath);
 
-                return push(ref(getDatabase(), 'inscricoes'), {
-                    ...formData,
-                    profileImageUrl: snapshot.ref.fullPath
-                });
-            })
-            .then(() => {
-                console.log('Dados Salvos no Firebase');
-                setFormData({
-                    profileImage: null,
-                    name: '',
-                    doc: '',
-                    phone: '',
-                    birthday: '',
-                    church: '',
-                    imageUrl: '',
-                    firstTime: '',
-                    transport: '',
-                    pranks: '',
-                    remedy: '',
-                    allergic: '',
-                    payment: '',
+        if (formData.name !== '' && formData.phone !== '' && formData.doc !== '' && formData.payment !== '') {
+
+            uploadBytes(storageRefe, formData.profileImage)
+                .then((snapshot) => {
+                    console.log('Imagem de perfil enviada com sucesso:', snapshot.ref.fullPath);
+
+                    return push(ref(getDatabase(), 'inscricoes'), {
+                        ...formData,
+                        profileImageUrl: snapshot.ref.fullPath
+                    });
                 })
-            }).catch(error => {
-                console.error('Erro ao enviar imagem de perfil ou salvar dados no Firebase:', error)
-            });
+                .then(() => {
+                    console.log('Dados Salvos no Firebase');
+                    setFormData({
+                        profileImage: null,
+                        name: '',
+                        doc: '',
+                        phone: '',
+                        birthday: '',
+                        church: '',
+                        imageUrl: '',
+                        firstTime: '',
+                        transport: '',
+                        pranks: '',
+                        remedy: '',
+                        allergic: '',
+                        payment: '',
+                    })
+                    navigate("/thanks")
+                }).catch(error => {
+                    console.error('Erro ao enviar imagem de perfil ou salvar dados no Firebase:', error)
+                });
+
+        }
+
 
         /* firebase.database().ref('inscricoes').push(formData)
             .then(() => {
@@ -113,6 +128,11 @@ function RegistrationForm() {
     return (
         <div className='App'>
             <div className='container'>
+
+                <div className='logo'>
+                    <img src={logo} alt="" />
+                </div>
+
                 <div>
                     <Title title="Preencha os campos abaixo e se inscreva pro nosso acampa!" />
                 </div>
@@ -122,19 +142,25 @@ function RegistrationForm() {
                     <div className='azul-claro'>
 
                         <div className='verde-claro'>
-                            {formData.imageUrl && (
-                                <div>
-                                    <img src={formData.imageUrl} alt="Imagem Perfil" className='image' />
-                                </div>
-                            )}
+
                             <label>
-                                Insira sua foto aqui
+                                <span>
+                                    <FiUpload color="#FFF" size={25} />
+
+                                </span>
+                                <input
+                                    type="file"
+                                    name="profileImage"
+                                    onChange={handleChange}
+                                />
+                                {formData.imageUrl ? (
+                                    <img src={formData.imageUrl} alt="Imagem Perfil" className='image' />
+                                ) : (
+                                    <img src={avatar} alt='Imagem Perfil' className='image' />
+                                )}
+                                <p>Clique pra inserir sua foto</p>
                             </label>
-                            <input
-                                type="file"
-                                name="profileImage"
-                                onChange={handleChange}
-                            />
+
                         </div>
 
                         <div className="user-data">
@@ -209,103 +235,108 @@ function RegistrationForm() {
 
                     </div>
 
+                    <div className='laranja'>
+                        <div className='roxo'>
+                            <p>É a primeira vez que acampa com a gente?</p>
+                            <div className='form-input'>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="firstTime"
+                                        value={formData.firstTime}
+                                        checked={formData.firstTime === "Sim"}
+                                        onChange={() => handleRadioChange("firstTime", "Sim")}
 
-
-
-
-
-
-
-
-                    <div className=''>
-                        É a primeira vez que acampa com a gente?
-                        <div className='radio-inputs'>
-                            <label >
-                                <input
-                                    type="radio"
-                                    name="firstTime"
-                                    value={formData.firstTime}
-                                    checked={formData.firstTime === "Sim"}
-                                    onChange={() => handleRadioChange("firstTime", "Sim")}
-
-                                /> Sim
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    name="firstTime"
-                                    value={formData.firstTime}
-                                    checked={formData.firstTime === "Não"}
-                                    onChange={() => handleRadioChange("firstTime", "Não")}
-                                />
-                                Não
-                            </label>
+                                    /> Sim
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="firstTime"
+                                        value={formData.firstTime}
+                                        checked={formData.firstTime === "Não"}
+                                        onChange={() => handleRadioChange("firstTime", "Não")}
+                                    />
+                                    Não
+                                </label>
+                            </div>
                         </div>
+
+
+
+                        <div className='verde-agua'>
+                            <p>Você precisará de transporte para a chacará?</p>
+                            <div className='form-input'>
+                                <label >
+                                    <input
+                                        type="radio"
+                                        name="transport"
+                                        value={formData.transport}
+                                        checked={formData.transport === "Sim"}
+                                        onChange={() => handleRadioChange("transport", "Sim")}
+
+                                    /> Sim
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="transport"
+                                        value={formData.transport}
+                                        checked={formData.transport === "Não"}
+                                        onChange={() => handleRadioChange("transport", "Não")}
+                                    />
+                                    Não
+                                </label>
+                            </div>
+                        </div>
+
+
+
+
+                        <div className='vermelho'>
+                            <p>Você topa participar de todas as brincadeiras?</p>
+                            <div className="form-input">
+                                <label >
+                                    <input
+                                        type="radio"
+                                        name="pranks"
+                                        value={formData.pranks}
+                                        checked={formData.pranks === "Sim"}
+                                        onChange={() => handleRadioChange("pranks", "Sim")}
+
+                                    /> Sim
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="pranks"
+                                        value={formData.pranks}
+                                        checked={formData.pranks === "Não"}
+                                        onChange={() => handleRadioChange("pranks", "Não")}
+                                    />
+                                    Não
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="pranks"
+                                        value={formData.pranks}
+                                        checked={formData.pranks === "Depende"}
+                                        onChange={() => handleRadioChange("pranks", "Depende")}
+                                    />
+                                    Depende
+                                </label>
+                            </div>
+                        </div>
+
+
                     </div>
 
-                    <div className=''>
-                        Você precisará de transporte para a chacará?
-                        <div className='radio-inputs'>
-                            <label >
-                                <input
-                                    type="radio"
-                                    name="transport"
-                                    value={formData.transport}
-                                    checked={formData.transport === "Sim"}
-                                    onChange={() => handleRadioChange("transport", "Sim")}
 
-                                /> Sim
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    name="transport"
-                                    value={formData.transport}
-                                    checked={formData.transport === "Não"}
-                                    onChange={() => handleRadioChange("transport", "Não")}
-                                />
-                                Não
-                            </label>
-                        </div>
-                    </div>
 
-                    <div className=''>
-                        Você topa participar de todas as brincadeiras?
-                        <div className='radio-inputs'>
-                            <label >
-                                <input
-                                    type="radio"
-                                    name="pranks"
-                                    value={formData.pranks}
-                                    checked={formData.pranks === "Sim"}
-                                    onChange={() => handleRadioChange("pranks", "Sim")}
 
-                                /> Sim
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    name="pranks"
-                                    value={formData.pranks}
-                                    checked={formData.pranks === "Não"}
-                                    onChange={() => handleRadioChange("pranks", "Não")}
-                                />
-                                Não
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    name="pranks"
-                                    value={formData.pranks}
-                                    checked={formData.pranks === "Depende"}
-                                    onChange={() => handleRadioChange("pranks", "Depende")}
-                                />
-                                Depende
-                            </label>
-                        </div>
-                    </div>
 
-                    <div className=''>
+                    <div className='pink'>
                         <label>
                             Você toma algum remédio diariamente?  Se sim, qual remédio?
                         </label>
@@ -317,7 +348,7 @@ function RegistrationForm() {
                         />
                     </div>
 
-                    <div className=''>
+                    <div className='azul-marinho'>
                         <label>
                             É alérgico a algum alimento? Se sim, quais alimentos?
                         </label>
@@ -329,36 +360,52 @@ function RegistrationForm() {
                         />
                     </div>
 
-                    <div className=''>
+                    <div className='white'>
                         <p>ATENÇÃO! Menores de 18 anos, clique no botão para baixar a ficha de autorização!</p>
-                        <button>Baixar Ficha</button>
+                        <a href="AUTACAMP" download="AUTACAMP"><button type='button'>Baixar Ficha</button></a>
                     </div>
 
-                    <div className=''>
-                        <label>
-                            Como você fará o pagamento? <p> **preços para apenas 1 pessoa, para promoções e descontos procure a liderança!</p>
-                        </label>
+                    <div className='green'>
+                        <div className='payment'>
+                            <label>
+                                Como você fará o pagamento?
+                            </label>
 
-                        <select
-                            name="payment"
-                            value={formData.payment}
-                            onChange={handleChange}
-                        >
-                            <option value="À vista">À vista</option>
-                            <option value="2x">2x R$125,00</option>
-                            <option value="3x">3x R$83,33</option>
-                            <option value="4x">4x R$62,50</option>
-                            <option value="5x">5x R$50,00</option>
-                            <option value="6x">6x R$41,66</option>
-                            <option value="7x">7x R$35,71</option>
-                            <option value="8x">8x R$31,25</option>
-                            <option value="9x">9x R$27,77</option>
-                            <option value="10x">10x de R$25,00</option>
-                        </select>
+                            <select
+                                name="payment"
+                                value={formData.payment}
+                                onChange={handleChange}
+                                required
+                            >
+                                <option value="À vista">À vista</option>
+                                <option value="2x">2x R$125,00</option>
+                                <option value="3x">3x R$83,33</option>
+                                <option value="4x">4x R$62,50</option>
+                                <option value="5x">5x R$50,00</option>
+                                <option value="6x">6x R$41,66</option>
+                                <option value="7x">7x R$35,71</option>
+                                <option value="8x">8x R$31,25</option>
+                                <option value="9x">9x R$27,77</option>
+                                <option value="10x">10x de R$25,00</option>
+                            </select>
+
+                            <p> **preços para apenas 1 pessoa, para promoções e descontos procure a liderança!</p>
+
+                        </div>
+
+                        <div className="whats">
+                            <p>Se precisar de ajuda para pagar, fale com a gente!</p>
+                            <a href='https://api.whatsapp.com/send?phone=5519989318887&text=Olá!%20Queria%20conversar%20sobre%20o%20Acampa20255.'
+                                target='_blank' rel='noreferrer'>Nosso Whatsapp</a>
+                        </div>
 
 
                     </div>
-                    <button type="submit">Me inscrever</button>
+                    {/* <button type="submit">Me inscrever</button> */}
+                    <div className="button2">
+                        <Button children="Me inscrever" type="submit" />
+                    </div>
+
                 </form>
             </div>
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from '../pages/Home'
 import Registration from '../pages/Registration'
+import Thanks from "../pages/Thanks";
 
 function RoutesApp() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Registration />} ></Route>
+            <Route path="/thanks" element={<Thanks />} />
         </Routes>
     )
 }
